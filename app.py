6833a8cc679ea20577179dbf89dff6c94e4027cc
gitusser-coder <p1234ppm@yahoo.com>
2025-08-08 23:18:26 +0200

    Update app.py
    json daten hinzugefügt in app.py

--- app.py
+++ app.py
@@ -2,10 +2,63 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+NAMEN = [
+
+{
+  'id': 1,
+  'name': 'Darius',
+  'Beruf': 'Softwareentwickler',
+  'Alter': '24',
+  'Größe': '1.82'
+
+},
+
+{
+
+  'id': 2,
+  'name': 'Danial',
+  'Beruf': 'TikToker',
+  'Alter': '22',
+  'Größe': '1.71'
+
+},
+
+{
+
+  'id': 3,
+  'name': 'Timon',
+  'Beruf': 'TikToker',
+  'Alter': '22',
+  'Größe': '1.70'
+
+},
+
+{
+
+  'id': 4,
+  'name': 'Kourosch',
+  'Beruf': 'TikToker',
+  'Alter': '20',
+  'Größe': '1.70'
+
+},
+
+{
+
+  'id': 5,
+  'name': 'Nikan',
+  'Beruf': 'Darmstadt',
+  'Alter': '22',
+  'Größe': '1.69'
+
+}
+
+
+]
 
 @app.route('/')
 def index():
-  return render_template('index.html')
+  return render_template('index.html', namen=NAMEN, Geschäftsführer=Darius)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -17,6 +70,9 @@ def login():
 def pdf():
 
   return render_template("pdf.html")
+
+
+
 
 
 if __name__ == '__main__':
